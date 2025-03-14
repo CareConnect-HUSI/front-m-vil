24c0e78ac1235bd3622707b11b4b001b4c73c027
Axell5 <axelcaro034@gmail.com>
2025-03-14 13:41:53 -0500

    Correciones LoginActivity, Colores, Logo

--- app/src/main/java/com/example/careconnect/logInPage/LoginActivity.kt
+++ app/src/main/java/com/example/careconnect/logInPage/LoginActivity.kt
@@ -8,19 +8,20 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.careconnect.R
+import com.google.android.material.textfield.TextInputEditText
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        val emailInput = findViewById<EditText>(R.id.email_input)
-        val passwordInput = findViewById<EditText>(R.id.password_input)
+        val emailInput = findViewById<TextInputEditText>(R.id.email_edit_text)
+        val passwordInput = findViewById<TextInputEditText>(R.id.password_edit_text)
         val loginButton = findViewById<Button>(R.id.login_button)
         val forgotPassword = findViewById<TextView>(R.id.forgot_password)
 
         // Botón de iniciar sesión
-        loginButton.setOnClickListener {
+        loginButton.setOnClickListener  {
             val email = emailInput.text.toString()
             val password = passwordInput.text.toString()
 
